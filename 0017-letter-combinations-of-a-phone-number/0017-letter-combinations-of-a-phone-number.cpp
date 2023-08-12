5a5b0439351bf7cd6899642906114adeb02f126c
Uttam Mishra <112749489+Uttam8448@users.noreply.github.com>
2023-08-12 17:10:13 +0530

    Time: 0 ms (100%), Space: 6.5 MB (69.54%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 private:
-    void solve(string digits,string output,vector <string>& ans,int index,string mapping[]){
-        if(index>=digits.length()){
-            if(output.length()>0){
+    void solve(string digits,string output,vector <string>& ans,int index,vector<string>& mapping){
+        if(index >= digits.length()){
             ans.push_back(output);
-            }
             return;
         }
-        int number = digits[index]-'0';
-        string value=mapping [number];
+        int dig = digits[index]-'0';
+        string value = mapping[dig];
         for(int i=0;i<value.length();i++){
-            output.push_back(value[i]);
+            char ch = value[i];
+            output.push_back(ch);
             solve(digits,output,ans,index+1,mapping);
-            output.pop_back();      //backtracking removing of character mapped already
+            output.pop_back();
         }
 
     }
@@ -21,7 +20,10 @@ public:
         vector <string> ans;
         string output;
         int index=0;
-        string mapping[10] = {"","","abc","def","ghi","jkl","mno",  "pqrs","tuv","wxyz"};
+        vector<string> mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if(digits.length()==0){
+            return ans;
+        }
         solve(digits,output,ans,index,mapping);
         return ans;
     }
