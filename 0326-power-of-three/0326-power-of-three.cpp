687e3cfb24929b95ef9ea83254c429dfe423a399
Uttam Mishra <112749489+Uttam8448@users.noreply.github.com>
2023-09-16 10:05:39 +0530

    Time: 414 ms (5.42%), Space: 6.3 MB (10.88%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n%2==0){
-            return false;
-        }
-        if(n==3 || n==1){
-            return true;
-        }
-        else{
-            double c=1;
-            for(int i=1;i<sqrt(n);i++){
-                if(c*3==n){
+        double c=n;
+        for(int i=1;i<=sqrt(n)+1;i++){
+            if(c==1)
                     return true;
-                }
-                c=c*3;
+            c=c/3;
             }
-        }
         return false;
     }
 };
