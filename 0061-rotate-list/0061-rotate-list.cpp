452ec54f774225cdee90939c55391d79e74cf97a
Uttam Mishra <112749489+Uttam8448@users.noreply.github.com>
2023-10-05 12:15:29 +0530

    Time: 4 ms (69.41%), Space: 12.1 MB (14.79%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* helper(ListNode* head,ListNode* tail,ListNode* prev,int count){
+            if(tail==NULL){ return 0;}
+            helper(head,tail->next,tail,count);
+            if(count!=0){
+                tail->next=head;
+                prev->next=NULL;
+                head=tail;
+                count--;
+            }
+            return head;
+        
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        int count=1;
+        ListNode* tail=head;
+        if(head==NULL){
+            return NULL;
+        }
+        while(tail->next!=NULL){
+            count++;
+            tail=tail->next;
+        }
+        int actualRotateK = k % count;
+        if(actualRotateK==0 || k==0){
+            return head;
+        }
+        int newLastNodePos = count - actualRotateK - 1;
+        ListNode*newhead = nullptr;
+        ListNode* newLastNode = head;
+        for(int i=0;i<newLastNodePos;i++){
+            newLastNode=newLastNode->next;
+        }
+        tail->next=head;
+        head=newLastNode->next;
+        newLastNode->next=NULL;
+        return head;
+    }
+};
