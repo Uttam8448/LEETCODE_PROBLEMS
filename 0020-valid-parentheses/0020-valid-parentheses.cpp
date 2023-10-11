ce3ff26a85ba27b802315c31014ab1bdc75e395d
Uttam Mishra <112749489+Uttam8448@users.noreply.github.com>
2023-10-12 00:20:00 +0530

    Time: 0 ms (100%), Space: 7 MB (12.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,37 +2,30 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st1;
-        stack<char> st2;
-        stack<char> st3;
         int size=s.size();
         int i=0;
         while(i<size){
-            if(s[i]=='{'){
-                st1.push('{');
+            if(s[i]=='{' || s[i]=='(' || s[i]=='['){
+                st1.push(s[i]);
             }
-            else if(s[i]=='}'){
-                if(st1.empty()) return false;
-                if(st1.top()=='{')
-                    st1.pop();
-                else return false;
-            }
-            else if(s[i]=='('){
-                st1.push('(');
-            }
-            else if(s[i]==')'){
-                if(st1.empty()) return false;
-                if(st1.top()=='(')
-                    st1.pop();
-                else return false;
-            }
-            else if(s[i]=='['){
-                st1.push('[');
-            }
-            else if(s[i]==']'){
-                if(st1.empty()) return false;
-                if(st1.top()=='[')
-                    st1.pop();
-                else return false;
+            else{
+                if(st1.empty()){
+                    return false;
+                }
+                else{
+                    if(s[i]=='}')
+                        if(st1.top()=='{')
+                            st1.pop();
+                        else return false;
+                    if(s[i]==']')
+                        if(st1.top()=='[')
+                            st1.pop();
+                        else return false;
+                    if(s[i]==')')
+                        if(st1.top()=='(')
+                            st1.pop();
+                        else return false;
+                }
             }
             i++;
         }
