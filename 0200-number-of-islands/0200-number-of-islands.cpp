7453bf46798ac652d837c0de272950537d975309
Uttam Mishra <mishrauttam526@gmail.com>
2024-06-03 17:05:26 +0530

    Time: 28 ms (55.89%), Space: 15.6 MB (92.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-         void dfs(int i,int j,vector<vector<char>>&grid, int& rowSize,int& columnSize){
+        void dfs(int i,int j,vector<vector<char>>&grid, int& rowSize,int& columnSize){
         if(i<0 || j<0 || i>=rowSize || j>=columnSize){
             return;
         }
         if(grid[i][j]=='1'){
             grid[i][j]='0';
-            
             dfs(i-1,j,grid,rowSize,columnSize);
             dfs(i,j-1,grid,rowSize,columnSize);
             dfs(i,j+1,grid,rowSize,columnSize);
