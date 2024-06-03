baf4e55096ad2f30dcc9d73c2a56e7dc9c82b61b
Uttam Mishra <mishrauttam526@gmail.com>
2024-06-03 17:04:54 +0530

    Time: 31 ms (36.81%), Space: 15.8 MB (74.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-        void dfs(int i,int j,vector<vector<int>> &visited,vector<vector<char>>&grid, int& rowSize,int& columnSize){
+         void dfs(int i,int j,vector<vector<char>>&grid, int& rowSize,int& columnSize){
         if(i<0 || j<0 || i>=rowSize || j>=columnSize){
             return;
         }
-        if(visited[i][j]==0 && grid[i][j]=='1'){
-            visited[i][j]=1;
-            dfs(i-1,j,visited,grid,rowSize,columnSize);
-            dfs(i,j-1,visited,grid,rowSize,columnSize);
-            dfs(i,j+1,visited,grid,rowSize,columnSize);
-            dfs(i+1,j,visited,grid,rowSize,columnSize);
+        if(grid[i][j]=='1'){
+            grid[i][j]='0';
+            
+            dfs(i-1,j,grid,rowSize,columnSize);
+            dfs(i,j-1,grid,rowSize,columnSize);
+            dfs(i,j+1,grid,rowSize,columnSize);
+            dfs(i+1,j,grid,rowSize,columnSize);
         }
         else{
             return;
@@ -20,12 +21,11 @@ public:
         int count=0;
         int rowSize=grid.size();
         int columnSize=grid[0].size();
-        vector<vector<int>> visited(rowSize,vector<int>(columnSize,0));
         for(int i=0;i<rowSize;i++){
             for(int j=0;j<columnSize;j++){
-                if(grid[i][j]=='1' && visited[i][j] == 0 ){
+                if(grid[i][j]=='1'){
                     count++;
-                    dfs(i,j,visited,grid,rowSize,columnSize);
+                    dfs(i,j,grid,rowSize,columnSize);
                 }
             }
         }
